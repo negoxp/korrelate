25046fe0ab0809bb512eddad71cb84e416b88cab
Fernando Castillo MacbookAir <jfcr2204@hotmail.com>
2013-07-25 11:04:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Korrelate::Application.config.secret_token = '1aa12c385700c6b79a8fa6847f0d22d26264f33ae2cf366e2c435f959583dcec3ad933eb021040f8313a93e23ed2cb615e883e4089e69047ff4bf8ef999fc743'
