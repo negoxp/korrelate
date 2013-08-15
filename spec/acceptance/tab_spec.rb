5bbc05f17047ceffe7e07c62ad44d7d5a8945802
Sergio Aguirre <sergio@ievolutioned.com>
2013-08-14 21:05:13 -0500

    remove unused scenario

--- spec/acceptance/tab_spec.rb
+++ spec/acceptance/tab_spec.rb
@@ -14,21 +14,6 @@ feature 'Tab', %q{
 	  	find '#tab-dashboard[aria-hidden=true]'
 	end
 
-=begin
-	scenario 'Click on Audience must show Optimization & Browser (select input) without Online input text', :js=>true do
-		visit '/'
-	  	click_link 'Audience'	  
-
-	  	within('#tab-audience') do
-	  		click_link 'Impressions'
-			within('#tab-audience-impressions') do
-				find '[name=optimization]'
-				find '[name=browser]'
-				page.should have_no_selector '[name=online]'
-			end
-		end
-	end
-=end
 
 	scenario 'Click on Dashboard should not show Optimization Target (select input)', :js=>true do
 		visit '/'
